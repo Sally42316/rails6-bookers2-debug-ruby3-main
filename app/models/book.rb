d0eb1df5474b8cc0693690329fc836b63d309f0b
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-12-02 13:48:34 +0000

    12/2 22:50

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)

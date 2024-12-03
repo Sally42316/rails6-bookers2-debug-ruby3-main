7b03da1e25ef46ef351687c1f2d62e25b24f72e6
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-12-03 13:49:42 +0000

    12/3 23:01

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,31 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books.order(id: :desc)
     @book = Book.new
+    
+    #過去の投稿数カウントに使用 
     @today_book =  @books.created_today
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
+
+    # 1対多のDM
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu| 
+        @userEntry.each do |u| 
+          if cu.room_id == u.room_id 
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def search

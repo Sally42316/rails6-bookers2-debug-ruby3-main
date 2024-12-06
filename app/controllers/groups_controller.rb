b321e64a39ed4a1a592dfd57e4d949e1d6505c36
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-12-06 14:33:21 +0000

    12/6 プライベートメアド

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,12 @@ class GroupsController < ApplicationController
       @group = Group.find(params[:id])
       @user = User.find(params[:id])
     end
+
+    def send_mail(mail_title,mail_content,group_users) #メソッドに対して引数を設定
+      @mail_title = mail_title
+      @mail_content = mail_content
+      mail bcc: group_users.pluck(:email), subject: mail_title
+      end
   
     def new
       @group = Group.new
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  # 全メーラー共通の設定。デフォルトのままでOK
   default from: 'from@example.com'
   layout 'mailer'
 end

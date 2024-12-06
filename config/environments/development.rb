84bfd775ace3df7fb89509caa640470178a632a8
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-12-06 14:33:45 +0000

    12/6 プライベートメアドなし

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,7 +80,7 @@ config.action_mailer.smtp_settings = {
  address:              'smtp.gmail.com',
  port:                  587,
  domain:               'gmail.com',
- user_name:            'hami.sc042316@gmail.com',
+ user_name:            'メアド',
  password:             'アプリパスワードを記入',
  authentication:       'login',
  enable_starttls_auto:  true

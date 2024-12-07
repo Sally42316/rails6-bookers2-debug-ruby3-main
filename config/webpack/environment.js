5b9060a5c7509c92dac974bfd913fae8b34caf73
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-12-07 10:51:21 +0000

    12/7 ５つ星評価設定中

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -10,4 +10,14 @@ environment.plugins.prepend(
   })
 )
 
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+  })
+)
+
 module.exports = environment
